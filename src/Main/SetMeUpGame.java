d7c32f20dda31b2fea3c07d5f00cee22cc078938
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2022-04-11 10:47:58 +0300

    Start the strcture of my game

---
+++ src/Main/SetMeUpGame.java
@@ -0,0 +1,52 @@
+package Main;
+
+public class SetMeUpGame
+{
+	
+	
+	
+	
+	public enum TableSize
+	{
+		BIG(5),
+		SMALL(3);
+		private int m_value;
+		private TableSize(int i_value)
+		{
+			m_value = i_value;
+		}
+		public int GetValue()
+		{
+			return m_value;
+		}
+		
+	}
+	public enum MarbleColor
+	{
+		GREEN,
+		BLUE,
+		YELLOW,
+		NONE
+	}
+	
+	
+	
+	
+	
+	
+	private MarbleColor[][] m_board;
+	private AlgorithmsProvider m_algorithms_provider;
+	
+	public SetMeUpGame(String input_file)
+	{
+		
+	}
+	
+	
+	@Override
+	public String toString()
+	{
+		return null;
+	}
+
+}
---
+++ src/Main/SetMeUpState.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class SetMeUpState implements GameState
+{
+
+	public SetMeUpState()
+	{
+		
+	}
+
+}
